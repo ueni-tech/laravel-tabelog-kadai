01114cd8286844fc84049edafcb25977f90b0467
ueni.tech <ueni.tech@gmail.com>
2023-11-03 20:28:16 +0900

    #1 アイコン設定

--- kadai_002/nagoyameshi/resources/views/layouts/footer.blade.php
+++ kadai_002/nagoyameshi/resources/views/layouts/footer.blade.php
@@ -1,9 +1,11 @@
-<footer class="bg-light p-4">
+<footer class="bg-light p-2">
     <div class="container">
-      <div class="d-flex justify-content-center mb-3">
-        <a href="#" class="navbar-brand fs-3">NAGOYAMESHI</a>
+      <div class="d-flex justify-content-center mb-1">
+      <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+        <img src="{{asset('img/page-icon.png')}}" alt="" width="20" height="20" class="me-1"><span class="fs-4">{{ config('app.name', 'Laravel') }}</span> 
+      </a>
       </div>
-      <div class="d-flex justify-content-center mb-3">
+      <div class="d-flex justify-content-center mb-1">
         <a href="#" class="link-secondary me-3">会社概要</a>
         <a href="#" class="link-secondary">利用規約</a>
       </div>
