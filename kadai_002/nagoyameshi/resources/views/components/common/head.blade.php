21dbcdc0c6fd52c420da9831fa50cf164c466e4f
ueni.tech <ueni.tech@gmail.com>
2023-11-19 08:42:41 +0900

    #1 店舗詳細ページ作成

--- kadai_002/nagoyameshi/resources/views/components/common/head.blade.php
+++ kadai_002/nagoyameshi/resources/views/components/common/head.blade.php
@@ -14,9 +14,10 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
     <!-- Styles -->
     <link href="{{ asset('/css/ress.min.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
--- kadai_002/nagoyameshi/resources/views/restaurants/show.blade.php
+++ kadai_002/nagoyameshi/resources/views/restaurants/show.blade.php
@@ -1,6 +1,88 @@
 @extends('layouts.app')
 @section('content')
-<div>
-  {{ $restaurant->name }}
+<div class="restaurant-page">
+  <div class="w-50 m-auto py-4">
+    <div class="text-center mb-2">
+      <h1>{{$restaurant->name}}</h1>
+      <p>★評価</p>
+    </div>
+    <div class="mb-2">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a href="{{ route('restaurants.show', $restaurant) }}" class="nav-link active text-white">トップ</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark border">予約</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link text-dark border">レビュー</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <div class="mb-2">
+        <img src="{{ asset('/storage/img/restaurant_images/' . $restaurant->image) }}" alt="" class="restaurant-image">
+      </div>
+      <div class="container">
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">店舗名</span>
+          </div>
+          <div class="col">
+            <span>{{$restaurant->name}}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">説明</span>
+          </div>
+          <div class="col">
+            <span>{{$restaurant->description}}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">郵便番号</span>
+          </div>
+          <div class="col">
+            <span>{{$restaurant->postal_code}}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">住所</span>
+          </div>
+          <div class="col">
+            <span>{{$restaurant->address}}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">営業時間</span>
+          </div>
+          <div class="col">
+            <span>{{$restaurant->opening_time->format('H:i')}} ～ {{$restaurant->closing_time->format('H:i')}}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">定休日</span>
+          </div>
+          <div class="col">
+            <span>{{ $restaurant->regular_holidays->implode('day', '、') }}</span>
+          </div>
+        </div>
+        <div class="row mb-2 pb-2 border-bottom">
+          <div class="col-3">
+            <span class="fw-bold">カテゴリ</span>
+          </div>
+          <div class="col">
+            <span>{{ $restaurant->categories->implode('name', '、') }}</span>
+          </div>
+        </div>
+        <div class="btn btn-outline-primary">お気に入りボタン</div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
