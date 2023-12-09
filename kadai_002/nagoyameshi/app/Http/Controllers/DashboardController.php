69896fcf782c3717d86ede8e11fb09beefc8691c
ueni.tech <ueni.tech@gmail.com>
2023-12-09 12:51:18 +0900

    #1 管理者側　トップページ作成

--- kadai_002/nagoyameshi/app/Http/Controllers/DashboardController.php
+++ kadai_002/nagoyameshi/app/Http/Controllers/DashboardController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $users_total_count = User::count();
+        $users = User::all();
+        $subscribed_users = [];
+        foreach ($users as $user) {
+            if ($user->subscribed('default')) {
+                $subscribed_users[] = $user;
+            }
+        }
+        $users_subscribed_count = count($subscribed_users);
+
+        $restaurants_total_count = Restaurant::count();
+
+        $reservations_this_month_count = Reservation::whereMonth('reserved_datetime', date('m'))->count();
+
+        return view('dashboard.index', compact('users_total_count', 'users_subscribed_count', 'restaurants_total_count', 'reservations_this_month_count'));
     }
 }
--- kadai_002/nagoyameshi/resources/views/dashboard/index.blade.php
+++ kadai_002/nagoyameshi/resources/views/dashboard/index.blade.php
@@ -1,4 +1,94 @@
 @extends('layouts.dashboard')
-
 @section('content')
+<div class="container w-75">
+
+  <h2 class="mb-4">サマリー</h2>
+
+  <div class="card mb-3">
+    <div class="card-header">
+      <h3 class="card-title">会員数概要</h3>
+    </div>
+    <div class="card-body">
+      <div class="d-flex gap-2">
+        <div class="card col-3">
+          <div class="card-header">
+            <span class="card-title">総会員数</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end">{{$users_total_count}} 名</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            <span class="card-title">無料会員数</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end">{{$users_total_count - $users_subscribed_count}} 名</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            <span class="card-title">有料会員数</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end">{{$users_subscribed_count}} 名</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card mb-3">
+    <div class="card-header">
+      <h3 class="card-title">店舗概要</h3>
+    </div>
+    <div class="card-body">
+      <div class="d-flex gap-2">
+        <div class="card col-3">
+          <div class="card-header">
+            <span class="card-title">登録店舗数</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end">{{$restaurants_total_count}} 店舗</p>
+          </div>
+        </div>
+        <div class="card col-3">
+          <div class="card-header">
+            <span class="card-title">当月総予約数</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end">{{$reservations_this_month_count}} 件</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card mb-3">
+    <div class="card-header">
+      <h3 class="card-title">サブスクリプション概要</h3>
+    </div>
+    <div class="card-body">
+      <div class="d-flex gap-2">
+        <div class="card col-4">
+          <div class="card-header">
+            <span class="card-title">当月サブスク合計額</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end"> 円</p>
+          </div>
+        </div>
+        <div class="card col-4">
+          <div class="card-header">
+            <span class="card-title">月別サブスク合計額</span>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-end"> 円</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
 @endsection
