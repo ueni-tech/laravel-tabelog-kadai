46644a04bed4ac0cb21e9a3ec61b2930dffcbd3c
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-15 13:06:05 +0900

    #1 herokuへのデプロイの設定

--- kadai_002/nagoyameshi/app/Providers/AppServiceProvider.php
+++ kadai_002/nagoyameshi/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,5 +36,9 @@ class AppServiceProvider extends ServiceProvider
         Validator::replacer('custom_email', function ($message, $attribute, $rule, $parameters) {
             return str_replace(':attribute', $attribute, 'メールアドレスは正しい形式で入力してください。');
         });
+
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
